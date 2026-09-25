2477237de25db4ada4f2eea9c6bdeadcffa8b51b
IDA PUTU MAHENDRA SUBALI <4gus081@gmail.com>
2026-09-25 10:22:34 +0800

    Update form field IDs for surat number

--- script.js
+++ script.js
@@ -487,7 +487,7 @@ function fillFormFromExtractedSurat(d){
   document.getElementById('f-pemohon').value = 'Team Sound';
   if(d.acara) document.getElementById('f-acara').value = d.acara;
   if(d.tempat) document.getElementById('f-tempat').value = d.tempat;
-  if(d.nomor_surat) document.getElementById('f-surat').value = d.nomor_surat;
+  if(d.nomor_surat) document.getElementById('f-suratmasuk').value = d.nomor_surat;
   checkDateStatus();
 }
 
